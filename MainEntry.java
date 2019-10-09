a9272b0d437022801576c34bfeeda34c5b8450e3
cellson7170 <55819943+cellson7170@users.noreply.github.com>
2019-10-08 22:35:04 -0700

    Add files via upload

---
+++ MainEntry.java
@@ -0,0 +1,34 @@
+//HEADER
+//Program Name:2D Array
+//Author: Chris Ellson (actually Mr. Burke, with input from students)
+//Class: CS260 Fall 2019
+//Date: 10/08/2019
+//Description: Creates a 2D array and fills it dynamically to create
+//			   a 2D image of a triangle and a wine glass.
+
+package cs260_wk3_twoDArray;
+/**
+ * Main Entry for 2D Array program
+ * 
+ */
+public class MainEntry {
+
+	public static void main(String[] args) {
+		/**
+		 * Create and fill 2D array and print to screen
+		 */
+		TwoDArray arr = new TwoDArray();
+		arr.printArray();
+	}
+}
+
+/* FOOTER
+*       ********
+**       ****** 
+***       ****  
+****       **   
+*****      **   
+******     **   
+*******    **   
+********  ****  
+*/
